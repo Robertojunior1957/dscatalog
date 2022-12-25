a54e56cc8cbd0e46a186a30bcac0a4d390b80d30
junior <raneymaney_@outlook.com>
2022-12-25 14:28:21 -0300

    h2-console

---
+++ backend/src/main/java/com/livink/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.livink.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.livink.dscatalog.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
