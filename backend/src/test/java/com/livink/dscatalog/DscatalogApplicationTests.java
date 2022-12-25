3394743032b444b74e85e6ff4800effc089c4f9a
junior <raneymaney_@outlook.com>
2022-12-25 12:40:56 -0300

    backend project created

---
+++ backend/src/test/java/com/livink/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livink.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
