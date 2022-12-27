80374eae1ccb1bea7cf1f4b0f8b139171c426097
junior <raneymaney_@outlook.com>
2022-12-26 21:22:37 -0300

    insert category

--- backend/src/main/java/com/livink/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/livink/dscatalog/resources/CategoryResource.java
@@ -1,15 +1,20 @@
 package com.livink.dscatalog.resources;
 
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.livink.dscatalog.entities.Category;
+import com.livink.dscatalog.dto.CategoryDTO;
 import com.livink.dscatalog.services.CategoryService;
 
 @RestController
@@ -20,7 +25,25 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> finAll(){
+	public ResponseEntity<List<CategoryDTO>> finAll(){
 		return ResponseEntity.ok(service.findAll());
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> finById(@PathVariable Long id){
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.ok(dto);
+	}
+	
 }
+
+
--- backend/src/main/java/com/livink/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/livink/dscatalog/services/CategoryService.java
@@ -1,12 +1,17 @@
 package com.livink.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.livink.dscatalog.dto.CategoryDTO;
 import com.livink.dscatalog.entities.Category;
 import com.livink.dscatalog.repositories.CategoryRepository;
+import com.livink.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -14,7 +19,25 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
-	public List<Category> findAll(){
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		List<CategoryDTO> listDTO = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		return listDTO;
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entidade nao encontrada para o id "+id));
+		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return null;
 	}
 }
